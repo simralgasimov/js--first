0d7698f051c8b87e26216bb08ea9744f68a4927c
simralgasimov <simralgasimov@gmail.com>
2023-10-23 01:05:01 +0400

    js

---
+++ index.js
@@ -0,0 +1,15 @@
+let a=prompt('sayi daxil edin')
+if(a % 4==0 && a % 6 ==0){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+let b=prompt('metni daxil edin')
+if(b.indexOf('e' , 5)){
+    console.log('yes');
+}
+else{
+    console.log('no');
+}
